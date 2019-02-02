06de976ec1faecf4e22fe3ec49edcf2326df7db0
Ivana <devision82@users.noreply.github.com>
2019-02-02 12:08:06 +0100

    Change Setup Function

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(400, 300);
+  background(200);
 
- function draw() {
-  background(005);
+  strokeWeight(4);
+
+  fill(0,170,0);
+  rect(width/2-50, height/2-50, 100, 100);
 }
