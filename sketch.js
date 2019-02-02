cd7c6214439a8d362bd96fb070a794748582a402
Ivana <devision82@users.noreply.github.com>
2019-02-02 12:26:25 +0100

    Update sketch.js
    Teeth added

--- sketch.js
+++ sketch.js
@@ -10,8 +10,15 @@ function setup() {
  ellipse(width/2-50+25, height/2-50+30, 20, 20)
  ellipse(width/2-50+70, height/2-50+30, 30, 30)
  line(width/2-30, height/2+30, width/2+30, height/2+30)
+ stroke(0);
+  line(width/2-30, height/2+30, width/2+30, height/2+30)
+  noStroke();
+  rect(width/2-30, height/2+18, 10, 10);
+  rect(width/2+20, height/2+18, 10, 10);
+  stroke(0);
   translate(width/2-60,height/2-60);
 fill(255,100,30);
  triangle(0, 0, 0, 60, 60, 0);
+ 
 
 }
