5ad19755f9b70f2bceb5c2e044bcbe55fd6129d8
Ivana <devision82@users.noreply.github.com>
2019-02-02 12:49:26 +0100

    Update sketch.js
    Push/Pop Mounth

--- sketch.js
+++ sketch.js
@@ -1,29 +1,60 @@
+var positionX = 0;
+var positionY = 0;
+
 function setup() {
   createCanvas(400, 300);
 }
 
 function draw() {
-background(200);
+  background(200);
 
-strokeWeight(2);
+  positionX = mouseX;
+  positionY = mouseY;
 
-fill(0,170,0);
-rect(mouseX-50, mouseY-50, 100, 100);
+  nacrtajTelo(positionX, positionY);
+  nacrtajOci();
+  nacrtajKapu();
 
-fill (255);
-ellipse(mouseX-50+25, mouseY-50+30, 20, 20)
-ellipse(mouseX-50+70, mouseY-50+30, 30, 30)
+  if (mouseIsPressed) {
+    nacrtajUsta(true);
+  } else {
+    nacrtajUsta(false);
+  }
+}
 
-stroke(0);
-line(mouseX-30, mouseY+30, mouseX+30, mouseY+30)
+function nacrtajTelo(x, y) {
+  strokeWeight(4);
+  fill(0,170,0);
+  rect(x - 50, y - 50, 100, 100);
+}
 
-noStroke();
-rect(mouseX-30, mouseY+18, 10, 10);
-rect(mouseX+20, mouseY+18, 10, 10);
+function nacrtajKapu() {
+  push();
+  stroke(0);
+  translate(positionX - 60, positionY - 60);
+  fill(255,100,30);
+  triangle(0, 0, 0, 60, 60, 0);
+  pop();
+}
 
-stroke(0);
-translate(mouseX-60,mouseY-60);
-fill(255,100,30);
-triangle(0, 0, 0, 60, 60, 0);
+function nacrtajOci() {
+  fill(255);
+  ellipse(positionX - 50 + 25, positionY - 50 + 30, 20, 20)
+  ellipse(positionX - 50 + 70, positionY - 50 + 30, 30, 30)
+}
 
+function nacrtajUsta(zatvorena) {
+  if (zatvorena) {
+    stroke(0);
+    line(positionX - 30, positionY + 30, positionX + 30, positionY + 30)
+  } else {
+    noStroke();
+    fill(255, 0, 0);
+    rect(positionX - 30, positionY + 13, 60, 20);
+  }
+
+  fill(255);
+  noStroke();
+  rect(positionX - 30, positionY + 18, 10, 10);
+  rect(positionX + 20, positionY + 18, 10, 10);
 }
