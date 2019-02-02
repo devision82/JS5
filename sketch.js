03d14a5cdb27c24c249d513a8b919deec5e29c03
Ivana <devision82@users.noreply.github.com>
2019-02-02 12:32:09 +0100

    Update sketch.js
    Mouse Movement

--- sketch.js
+++ sketch.js
@@ -1,24 +1,29 @@
 function setup() {
   createCanvas(400, 300);
-  background(200);
+}
 
-  strokeWeight(4);
+function draw() {
+background(200);
 
-  fill(0,170,0);
-  rect(width/2-50, height/2-50, 100, 100);
-  fill(255);
- ellipse(width/2-50+25, height/2-50+30, 20, 20)
- ellipse(width/2-50+70, height/2-50+30, 30, 30)
- line(width/2-30, height/2+30, width/2+30, height/2+30)
- stroke(0);
-  line(width/2-30, height/2+30, width/2+30, height/2+30)
-  noStroke();
-  rect(width/2-30, height/2+18, 10, 10);
-  rect(width/2+20, height/2+18, 10, 10);
-  stroke(0);
-  translate(width/2-60,height/2-60);
+strokeWeight(2);
+
+fill(0,170,0);
+rect(mouseX-50, mouseY-50, 100, 100);
+
+fill (255);
+ellipse(mouseX-50+25, mouseY-50+30, 20, 20)
+ellipse(mouseX-50+70, mouseY-50+30, 30, 30)
+
+stroke(0);
+line(mouseX-30, mouseY+30, mouseX+30, mouseY+30)
+
+noStroke();
+rect(mouseX-30, mouseY+18, 10, 10);
+rect(mouseX+20, mouseY+18, 10, 10);
+
+stroke(0);
+translate(mouseX-60,mouseY-60);
 fill(255,100,30);
- triangle(0, 0, 0, 60, 60, 0);
- 
+triangle(0, 0, 0, 60, 60, 0);
 
 }
